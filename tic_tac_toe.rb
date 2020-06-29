73e64a3b00537afeddd7bb5eb98f54c5aca11ba6
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2020-06-29 18:51:47 -0400

    Move .rb files to lib/, create .replit

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,10 +1,9 @@
 # Run this script to play the game
-require_relative 'print_helper'
-require_relative 'game'
-require_relative 'grid'
-require_relative 'player'
-require_relative 'mark'
-require_relative 'turn'
+require_relative 'lib/print_helper'
+require_relative 'lib/game'
+require_relative 'lib/grid'
+require_relative 'lib/player'
+require_relative 'lib/mark'
 
 include PrintHelper
 
